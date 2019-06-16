9b3ae8c21c7f9922ccb6531684568e513c5557b2
Marcelo Bidoli <mabidoli@gmail.com>
2019-06-16 08:15:55 -0300

    Initial commit

---
+++ kneat.swapi/Program.cs
@@ -0,0 +1,12 @@
+﻿using System;
+
+namespace kneat.swapi
+{
+    class Program
+    {
+        static void Main(string[] args)
+        {
+            Console.WriteLine("Hello World!");
+        }
+    }
+}
